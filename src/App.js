0cdbd871710a440f19f1f745ca987588382ef813
Jakub <jakub.bchyla@gmail.com>
2021-08-01 11:38:18 +0200

    last working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
+import React, { Component } from 'react';
+// import FirstComponent from './components/learing-examples/FirstComponent';
+// import SecondComponent from './components/learing-examples/SecondComponent';
+// import ThirdComponent from './components/learing-examples/ThirdComponent';
+
 import logo from './logo.svg';
 import './App.css';
+ 
+class App extends Component { 
+  render() {
+    return (
+      <div className="App">
+        My app
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
+
+
+
+// //Class Component
+// class FirstComponent extends Component {
+//   render() {
+//     return (
+//       <div className="firstComponent">
+//         FirstComponent
+//       </div>
+//     );
+//   }
+// }
+
+// class SecondComponent extends Component {
+//   render() {
+//     return (
+//       <div className="secondComponent">
+//         SecondComponent
+//       </div>
+//     );
+//   }
+// }
+
+// function ThirdComponent() {
+//   return (
+//     <div className="thirdComponent">
+//       ThirdComponent
+//     </div>
+//   );
+// }
+
+// function FourthComponent(){
+//   return(
+//     <div className="fourthComponent">
+//       FourthComponent
+//     </div>
+//   );
+// }
+
+
+
 
 export default App;
