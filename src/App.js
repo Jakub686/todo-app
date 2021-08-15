34f576110a05694ae917c2765a2649d5fde7ab2e
Jakub <jakub.bchyla@gmail.com>
2021-08-15 13:09:12 +0200

    last working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import FirstComponent from './components/learing-examples/FirstComponent';
 // import SecondComponent from './components/learing-examples/SecondComponent';
 // import ThirdComponent from './components/learing-examples/ThirdComponent';
-import Counter from './components/counter/Counter';
+//import Counter from './components/counter/Counter';
 import TodoApp from './components/todo/TodoApp';
 
 // import logo from './logo.svg';
